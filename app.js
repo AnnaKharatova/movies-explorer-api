277547412d257d117cbde8092eea5a2de35762d8
Anna Haratova <black-isida@yandex.ru>
2023-07-11 10:37:13 +0300

    cors

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ mongoose.connect(BD_ADDRESS, {
 app.use(express.json());
 app.use(requestLogger);
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://movies-express.nomoredomains.work');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 const allowedCors = [
   'https://movies-express.nomoredomains.work',
   'http://movies-express.nomoredomains.work',
