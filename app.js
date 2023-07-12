438eb552af50e1a811e6b302a62230d2e717820f
Anna Haratova <black-isida@yandex.ru>
2023-07-12 12:21:05 +0300

    fixed cors err

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(BD_ADDRESS, {
 app.use(express.json());
 app.use(requestLogger);
 
- const allowedCors = [
+/* const allowedCors = [
   'https://movies-express.nomoredomains.work',
   'http://movies-express.nomoredomains.work',
   'http://localhost:3006',
@@ -36,9 +36,9 @@ const corsOptions = {
   "allowedHeaders": 'Accept, Content-Type, Authorization',
   "optionsSuccessStatus": 204,
   "credentials": true,
-};
+}; */
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 
 app.use(limiter);
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const { JWT_SECRET = 'JWT_SECRET' } = process.env;
 const { BD_ADDRESS = 'mongodb://127.0.0.1/bitfilmsdb' } = process.env;
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 module.exports = {
   JWT_SECRET,
